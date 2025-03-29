3e5469b2df8b0c491ee0e340a11a27889d49902d
Rajat_Mishra <rajatmishra9411@gmail.com>
2025-03-30 01:25:49 +0530

    this is me

--- Kta_WebApp/src/App.jsx
+++ Kta_WebApp/src/App.jsx
@@ -1,7 +1,13 @@
-return (
-  <div>
-    <h1>KTA</h1>
-  </div>
-);
+import LandingPage from "./Components/LandingPage/LandingPage";
+import NavBar from "./Components/NavBar/NavBar";
+
+function App() {
+  return (
+    <div>
+      <NavBar/>
+      <LandingPage/>
+    </div>
+  );
+}
 
 export default App;
---
+++ Kta_WebApp/src/Components/LandingPage/LandingPage.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './LandingPage.scss'
+function LandingPage() {
+  return (
+    <div className='LandingPage'>
+
+
+
+      <div className='LandingLines'> 
+        <p className='LandingLineHeadding'>
+        Innovative Solutions in 
+        <p>Ceramics, Stone & Surface Care</p>
+        </p>
+        <p className='LandingLineSubHeadding'>High-quality materials and expert solutions for durability, aesthetics, and protection. 
+          <br />Trusted by professionals worldwide.</p>
+
+
+
+<div>
+  <img src="public/KTABanner.png" width={'50%'} alt="" />
+</div>
+
+      </div>
+    </div>
+  )
+}
+
+export default LandingPage
