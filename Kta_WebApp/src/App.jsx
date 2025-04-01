7ccdba068aa2405cda8524cbad1ac846cd7c7861
vishal kalwani <vishalkalwani623@gmail.com>
2025-04-02 00:27:41 +0530

    migrated all components to landing page and provided a react-container

--- Kta_WebApp/src/App.jsx
+++ Kta_WebApp/src/App.jsx
@@ -1,16 +1,9 @@
 import LandingPage from "./Pages/LandingPage/LandingPage";
-import NavBar from "./Components/NavBar/NavBar";
-import ContactNav from "./Components/ContactNav/ContactNav";
-import FadeSlider from "./Components/Slider/Slider";
 
 function App() {
   return (
     <div>
-      <ContactNav/>
-      <NavBar/>
-      <LandingPage/>
-      <FadeSlider/>
-
+      <LandingPage />
     </div>
   );
 }
--- Kta_WebApp/src/Components/ContactNav/ContactNav.jsx
+++ Kta_WebApp/src/Components/ContactNav/ContactNav.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './ContactNav.scss'
+import Container from 'react-bootstrap/esm/Container'
 function ContactNav() {
   return (
     <div className="contactNav">
-        
      <div> <img src="./contactIcon.svg" /> +91 94140-09900 </div> 
      <div> <img src="./mailIcon.svg" className='px-1' />  info@ktasolutions.in </div> 
     </div>
