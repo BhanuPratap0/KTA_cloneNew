9606a9a5756d9e211cbc657f3a92601e49885cf8
vishal kalwani <vishalkalwani623@gmail.com>
2025-04-06 21:47:06 +0530

    css optimised

--- Kta_WebApp/src/Pages/LandingPage/ShowCaseSection/ShowCaseSection.jsx
+++ Kta_WebApp/src/Pages/LandingPage/ShowCaseSection/ShowCaseSection.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { ProductList } from "@/Data/LandingPage";
 import ProductCard from "@/Components/ProductCard/ProductCard";
-import "./ShowCaseSection.scss";
+import styles from "./ShowCaseSection.module.scss";
 import HeadingComp from "@/Components/HeadingComp/HeadingComp";
 import GradientButton from "@/Components/GradientButton/GradientButton";
 const ShowCaseSection = () => {
   return (
-    <div className="showCaseContainer">
-      <div className="headings">
+    <div className={styles.ShowCaseContainer}>
+      <div className={styles.Headings}>
         <HeadingComp heading={"SHOWCASE"} subHeading={"Our Products"} />
-        <div className="subHeading">
+        <div className={styles.SubHeading}>
           Discover our range of industry-leading products.
         </div>
       </div>
-      <div className="productsContainer">
+      <div className={styles.ProductsContainer}>
         {ProductList.map((productItem, index) => (
           <ProductCard data={productItem} />
         ))}
