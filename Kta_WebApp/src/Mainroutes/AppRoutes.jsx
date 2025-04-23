986a9b229b7fe6e674ba95627312639719048c09
vishal kalwani <vishalkalwani623@gmail.com>
2025-04-24 01:24:45 +0530

    completed calculator page

--- Kta_WebApp/src/Mainroutes/AppRoutes.jsx
+++ Kta_WebApp/src/Mainroutes/AppRoutes.jsx
@@ -1,13 +1,18 @@
 import AboutPage from "@/Pages/AboutPage/AboutPage";
+import CalculatorPage from "@/Pages/CalculatorPage/CalculatorPage";
 import Products from "@/Pages/Products/Products";
 
 export const appRoutes = [
   {
     path: "about",
     element: <AboutPage />,
-  },{
+  },
+  {
     path: "products",
-    element:<Products/>
-  }
-
+    element: <Products />,
+  },
+  {
+    path: "Calculator",
+    element: <CalculatorPage />,
+  },
 ];
