135ae6828f9c93e9ff8ca156b4117952a268a834
vishal kalwani <vishalkalwani623@gmail.com>
2025-05-11 18:07:38 +0530

    footer's responsiveness fixed

--- Kta_WebApp/src/Components/TopBanner/TopBanner.jsx
+++ Kta_WebApp/src/Components/TopBanner/TopBanner.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import styles from './TopBanner.module.scss'
-function TopBanner({ head, details, detailsHeading }) {
-    return (
-        <div id={styles.TopBanner}>
-            <div className={styles.TopBannerHead}>
-                <img src={head} alt="" />
-            </div>
-            <div className={styles.TopBannerDetails}>
-                <div className={styles.TopBannerDetailsHead}>{detailsHeading}</div>
-                {details}</div>
-        </div>
-    )
+import React from "react";
+import styles from "./TopBanner.module.scss";
+function TopBanner({ head, highlight, details }) {
+  return (
+    <div id={styles.TopBanner}>
+      <div className={styles.TopBannerHead}>
+        <img src={head} alt="" />
+      </div>
+      <div className={styles.TopBannerDetails}>
+        <div className={styles.TopBannerHighlight}>{highlight} </div>
+        {details}
+      </div>
+    </div>
+  );
 }
 
-export default TopBanner
+export default TopBanner;
--- Kta_WebApp/src/Pages/LandingPage/FooterSection/FooterSection.jsx
+++ Kta_WebApp/src/Pages/LandingPage/FooterSection/FooterSection.jsx
@@ -7,81 +7,84 @@ import facebookIcon from "@/Assets/LandingPage/PlatFacebook.svg";
 import instagramIcon from "@/Assets/LandingPage/PlatInstagram.svg";
 import whatsappIcon from "@/Assets/LandingPage/PlatWhatsapp.svg";
 import ktaIcon from "@/Assets/LandingPage/KtaIcon.png";
+import { Container } from "react-bootstrap";
 const FooterSection = () => {
   return (
-    <div className={styles.FooterSection}>
-      <div className={styles.RightTopSection}>
-        <div>
-          <img src={facebookIcon} />
+    <Container style={{ maxWidth: "1440px" }}>
+      <div className={styles.FooterSection}>
+        <div className={styles.RightTopSection}>
+          <div>
+            <img src={facebookIcon} />
+          </div>
+          <div>
+            <img src={instagramIcon} alt="" />
+          </div>
+          <div>
+            <img src={whatsappIcon} alt="" />
+          </div>
         </div>
-        <div>
-          <img src={instagramIcon} alt="" />
+        <div className={styles.LeftSection}>
+          <div className={styles.Logo}>
+            <img src={ktaIcon} alt="" />
+          </div>
+          <div className={styles.Copyright}>
+            © Copyright KTA Solutions. All Rights Reserved Mfd. and Marketed by
+            <br />
+            SOBHRAJ DEVELOPMENTS PVT. LTD.
+          </div>
         </div>
-        <div>
-          <img src={whatsappIcon} alt="" />
+        <div className={styles.RightSection}>
+          <div className={styles.List}>
+            <span className={styles.Title}>Useful links</span>
+            <span>
+              <a>About Us</a>
+            </span>
+            <span>
+              <a>Product</a>
+            </span>
+            <span>
+              <a>Blogs</a>
+            </span>
+            <span>
+              <a>Careers</a>
+            </span>
+          </div>
+          <div className={styles.List}>
+            <span className={styles.Title}>Useful links</span>
+            <span>
+              <a>Terms & Conditions</a>
+            </span>
+            <span>
+              <a>Privacy Policy</a>
+            </span>
+            <span>
+              <a>Shipping & Returns</a>
+            </span>
+            <span>
+              <a>Contact</a>
+            </span>
+          </div>
+          <div className={styles.ContactUs}>
+            <span className={styles.Title}>Contact Us</span>
+            <span>
+              <MdMailOutline size={"20px"} />
+              info@ktasolutions.in
+            </span>
+            <span>
+              <BsTelephone size={"20px"} />
+              +91 94140-09900
+            </span>
+            <span>
+              <IoLocationOutline size={"20px"} />
+              <p>
+                Mangliawas - Nasirabad Road Mangliawas, Ajmer - 305203
+                Rajasthan, India.
+              </p>
+            </span>
+          </div>
         </div>
       </div>
-      <div className={styles.LeftSection}>
-        <div className={styles.Logo}>
-          <img src={ktaIcon} alt="" />
-        </div>
-        <div className={styles.Copyright}>
-          © Copyright KTA Solutions. All Rights Reserved Mfd. and Marketed by
-          <br />
-          SOBHRAJ DEVELOPMENTS PVT. LTD.
-        </div>
-      </div>
-      <div className={styles.RightSection}>
-        <div className={styles.List}>
-          <span className={styles.Title}>Useful links</span>
-          <span>
-            <a>About Us</a>
-          </span>
-          <span>
-            <a>Product</a>
-          </span>
-          <span>
-            <a>Blogs</a>
-          </span>
-          <span>
-            <a>Careers</a>
-          </span>
-        </div>
-        <div className={styles.List}>
-          <span className={styles.Title}>Useful links</span>
-          <span>
-            <a>Terms & Conditions</a>
-          </span>
-          <span>
-            <a>Privacy Policy</a>
-          </span>
-          <span>
-            <a>Shipping & Returns</a>
-          </span>
-          <span>
-            <a>Contact</a>
-          </span>
-        </div>
-        <div className={styles.ContactUs}>
-          <span className={styles.Title}>Contact Us</span>
-          <span>
-            <MdMailOutline size={"20px"} />
-            info@ktasolutions.in
-          </span>
-          <span>
-            <BsTelephone size={"20px"} />
-            +91 94140-09900
-          </span>
-          <span>
-            <IoLocationOutline size={"20px"} />
-            <p>
-              Mangliawas - Nasirabad Road Mangliawas, Ajmer - 305203 Rajasthan,
-              India.
-            </p>
-          </span>
-        </div>
-      </div>
-    </div>
+    </Container>
   );
 };
 
