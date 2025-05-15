ff0c0b3d4ffcae8978221dcb5969f5efd0bc60c5
vishal kalwani <vishalkalwani623@gmail.com>
2025-05-15 10:47:42 +0530

    landing page responsive

--- Kta_WebApp/src/Pages/LandingPage/ShowCaseSection/ShowCaseSection.jsx
+++ Kta_WebApp/src/Pages/LandingPage/ShowCaseSection/ShowCaseSection.jsx
@@ -5,45 +5,44 @@ import styles from "./ShowCaseSection.module.scss";
 import HeadingComp from "@/Components/HeadingComp/HeadingComp";
 import GradientButton from "@/Components/GradientButton/GradientButton";
 import Slider from "react-slick";
-import './ShowCaseSectionSlider.css'
+import "./ShowCaseSectionSlider.css";
 
 const ShowCaseSection = () => {
-
   const settings = {
     dots: false,
     infinite: true,
     speed: 500,
     slidesToShow: 4,
     slidesToScroll: 2,
-
   };
-
-
 
   return (
     <div id="show" className={styles.ShowCaseContainer}>
       <div className={styles.Headings}>
-        <HeadingComp heading={"SHOWCASE"} subHeading={"Our Products"} />
+        <HeadingComp heading={"Our Products"} subHeading={"showcase"} />
         <div className={styles.SubHeading}>
           Discover our range of industry-leading products.
         </div>
       </div>
-    
-
-      <div className="slider-container">
-        <Slider {...settings}>
-          {ProductList.map((productItem, index) => (
-            <ProductCard data={productItem} />
-          ))}
-
-        </Slider>
+      <div className={styles.ProductsContainerDesktop}>
+        <div className="slider-container">
+          <Slider {...settings}>
+            {ProductList.map((productItem, index) => (
+              <ProductCard data={productItem} />
+            ))}
+          </Slider>
+        </div>
       </div>
-       <div className={styles.fitButton}>
-      <GradientButton  text={"View All Products"} />
-        </div>   
+      <div className={styles.ProductsContainerMobile}>
+        {ProductList.map((productItem, index) => (
+          <ProductCard data={productItem} />
+        ))}
+      </div>
+      <div className={styles.fitButton}>
+        <GradientButton text={"View All Products"} />
+      </div>
     </div>
   );
 };
 
 export default ShowCaseSection;
-
